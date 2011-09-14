e767bf7087238d22351979728fa10d029d7dff7a
David Flanagan <dflanagan@mozilla.com>
2011-09-13 22:05:31 -0700

    bump version number

--- src/NodeApp.js
+++ src/NodeApp.js
@@ -15,7 +15,7 @@ var path = require("path");
 var console = require("console");
 var util = require("util");
 
-const VERSION = "0.12";
+const VERSION = "0.15";
 
 function usage(code) {
     var done = process.stdout.write(
